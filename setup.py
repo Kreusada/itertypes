e1b0c213cfa077b08ff948a9e95d040c4d5109fd
Kreusada <kreusadaprojects@gmail.com>
2021-08-04 13:29:44 +0100

    v1.3.4 Version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fp:
 
 setup(
     name="itertypes",
-    version="1.3.3",
+    version="1.3.4",
     author="Kreusada",
     author_email="kreusadaprojects@gmail.com",
     description="A small library used to obtain types for various builtin iterators.",
